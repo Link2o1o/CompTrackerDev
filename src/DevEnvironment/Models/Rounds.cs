47afffacd4f0ce31ce5ddf98dcd3cc7d7ab5d7ec
Michael <private1384@live.com>
2017-02-27 16:41:35 -0700

    Fixes Footer, Adjusts Rounds, Tournament shows a temp layout for 12 players

--- src/DevEnvironment/Models/Rounds.cs
+++ src/DevEnvironment/Models/Rounds.cs
@@ -12,10 +12,17 @@ namespace DevEnvironment.Models
         private List<Pairs> pairList = new List<Pairs>();
 
         public void addPair(string player1, string player2) {
-
+            Pairs temp = new Pairs();
+            temp.Player1 = player1;
+            temp.Player2 = player2;
+            temp.PairId = temp.PairId + 1;
+            pairList.Add(temp);
         }
         public void removePair(int pairId) {
-
+            pairList.RemoveAt(pairId);
+        }
+        public Pairs getPairs(int pairId) {
+            return pairList[pairId];
         }
     }
 }
