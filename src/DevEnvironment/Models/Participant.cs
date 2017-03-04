2ceaafaeb41b7e43a40560d32c492ed5922c4795
Michael <private1384@live.com>
2017-03-03 20:22:56 -0700

    Edits Participant, attempts to bind participant model with registration page

--- src/DevEnvironment/Models/Participant.cs
+++ src/DevEnvironment/Models/Participant.cs
@@ -8,29 +8,15 @@ namespace DevEnvironment
 {
     public class Participant
     {
-        public string Name;
-        public string Username;
-        public string Password;
-        public string Birthday;
-        public string Gender;
-        public string Email;
-        public string HSName;
-        public string NextCollege;
-        public string Awards;
-        public string Experience;
-
-        public void NP(string Name, string Username, string Password, string Birthday, string Gender, string Email, string HSName, string NextCollege, string Awards, string Experience) {
-            this.Name = Name;
-            this.Username = Username;
-            this.Password = Password;
-            this.Birthday = Birthday;
-            this.Gender = Gender;
-            this.Email = Email;
-            this.HSName = HSName;
-            this.NextCollege = NextCollege;
-            this.Awards = Awards;
-            this.Experience = Experience;
-        }
-        public 
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Birthday { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string HSName { get; set; }
+        public string NextCollege { get; set; }
+        public string Awards { get; set; }
+        public string Experience { get; set; }
     }
 }
