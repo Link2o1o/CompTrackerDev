c33641d8697ac90c8d8fc33c038bee3c21b5ac54
Michael <private1384@live.com>
2017-03-08 19:29:53 -0700

    Comments Registration Index page and Participant Model

--- src/DevEnvironment/Models/Participant.cs
+++ src/DevEnvironment/Models/Participant.cs
@@ -9,38 +9,61 @@ namespace DevEnvironment.Models
 {
     public class Participant
     {
+        //First name 
         [Required(ErrorMessage = "Please enter your name.")]
         [DataType(DataType.Text)]
         public string FirstName { get; set; }
+
+        //Last name
         [Required]
         [DataType(DataType.Text)]
         public string LastName { get; set; }
+
+        //Username
         [Required]
         [DataType(DataType.Text)]
         public string Username { get; set; }
+
+        //Password
         [Required]
         [DataType(DataType.Password)]
         [StringLength(64, MinimumLength = 6, ErrorMessage = "Password cannot be greater than 64, or less than 6.")]
         public string Password { get; set; }
-        [Required]
-        [DataType(DataType.Date)]
-        public DateTime Birthday { get; set; }
-        [Required]
-        public string Gender { get; set; }
-        [Required]
-        [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
-        [Required]
-        [DataType(DataType.Text)]
-        public string HSName { get; set; }
-        [DataType(DataType.Text)]
-        public string NextCollege { get; set; }
-        [DataType(DataType.MultilineText)]
-        public string Awards { get; set; }
-        [DataType(DataType.MultilineText)]
-        public string Experience { get; set; }
+
+        //Compares Password with this field
         [Required]
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
+
+        //Birthday
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        //Gender
+        [Required]
+        public string Gender { get; set; }
+
+        //Email
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        //High School name
+        [Required]
+        [DataType(DataType.Text)]
+        public string HSName { get; set; }
+
+        //Desired College
+        [DataType(DataType.Text)]
+        public string NextCollege { get; set; }
+
+        //Awards
+        [DataType(DataType.MultilineText)]
+        public string Awards { get; set; }
+
+        //Previous Experience
+        [DataType(DataType.MultilineText)]
+        public string Experience { get; set; }
     }
 }
