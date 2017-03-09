e809a3f525e6c286b1823a969a0250df777a01ea
Michael <private1384@live.com>
2017-03-08 19:06:33 -0700

    Updates Participant for Model binding and binds to registration index page

--- src/DevEnvironment/Models/Participant.cs
+++ src/DevEnvironment/Models/Participant.cs
@@ -1,22 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DevEnvironment
+namespace DevEnvironment.Models
 {
     public class Participant
     {
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your name.")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
         public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(64, MinimumLength = 6, ErrorMessage = "Password cannot be greater than 64, or less than 6.")]
         public string Password { get; set; }
-        public string Birthday { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        [Required]
         public string Gender { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
         public string HSName { get; set; }
+        [DataType(DataType.Text)]
         public string NextCollege { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Awards { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Experience { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
     }
 }
